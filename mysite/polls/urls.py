4978d875d7e6b5d719b440f8bd80bb3330c3178c
7000WORDS <emekadrake707@gmail.com>
2021-02-17 16:54:08 +0000

    anptheer issue cannot access some urls.

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -7,6 +7,7 @@ from .models import Question
 
 urlpatterns = [
     path ('', views.index, name = 'index'),
+    path('<int:question_id/', views.detail, name='detail'),
     path('<int:question_id/results/', views.results,name= 'results'),
     path('<int:question_id>/vote/vote', views.vote, name= 'vote'),
 ]
