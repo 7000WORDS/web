7c6f2746846654812721fccc2952e522bc4d554c
7000WORDS <emekadrake707@gmail.com>
2021-02-17 16:23:42 +0000

    django bro

---
+++ mysite/polls/urls.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.urls import path 
+
+from . import views
+from .models import Question
+
+
+urlpatterns = [
+    path ('', views.index, name = 'index'),
+    path('<int:question_id/results/', views.results,name= 'results'),
+    path('<int:question_id>/vote/vote', views.vote, name= 'vote'),
+]
